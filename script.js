e99063d5857a8c556a01406bcced39bf88efbed1
Mufida Salma <46271228+upida@users.noreply.github.com>
2020-06-06 11:30:18 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,74 @@
+var page = ['dimsum', 'katsu_tempe', 'martabak_manis'];
+
+var dimsum = [
+  'img/dimsum/IMG_20200409_094645_772.jpg',
+  'img/dimsum/IMG_20200409_094536_055.jpg',
+  'img/dimsum/IMG_20200409_094741_443.jpg',
+  'img/dimsum/IMG_20200409_094903_239.jpg',
+];
+var katsu_tempe = [
+  'img/katsu-tempe/widyamarinil_20200605_1.png',
+  'img/katsu-tempe/widyamarinil_20200605_2.png',
+  'img/katsu-tempe/widyamarinil_20200605_3.png',
+  'img/katsu-tempe/widyamarinil_20200605_4.png',
+];
+var martabak_manis = [
+  'img/martabak-manis/1.jpg',
+  'img/martabak-manis/2.jpg',
+  'img/martabak-manis/3.jpg',
+  'img/martabak-manis/4.jpg',
+  'img/martabak-manis/5.jpg',
+  'img/martabak-manis/6.jpg',
+  'img/martabak-manis/7.jpg',
+];
+var hal = 0;
+
+function change(x) {
+  $('#wrapper').fadeOut(500, function () {
+    $('#wrapper').fadeIn(500, function () {
+      $('#carousel').fadeIn('slow');
+      //kode lanjutan
+      if (page[x] == 'dimsum') {
+        let this_page = dimsum;
+        $('.nextpage').click(function () {
+          hal = 1;
+          change(hal);
+          console.log(hal);
+        });
+        images(this_page);
+      } else if (page[x] == 'katsu_tempe') {
+        let this_page = katsu_tempe;
+        $('.nextpage').click(function () {
+          hal = 2;
+          change(hal);
+          console.log(hal);
+        });
+        images(this_page);
+      } else if (page[x] == 'martabak_manis') {
+        let this_page = martabak_manis;
+        $('.nextpage').click(function () {
+          hal = 0;
+          change(hal);
+          console.log(hal);
+        });
+        images(this_page);
+      }
+    });
+    if (x == 0) {
+      $('#wrapper').load('dimsum.html');
+    } else if (x == 1) {
+      $('#wrapper').load('katsu-tempe.html');
+    } else if (x == 2) {
+      $('#wrapper').load('martabak-manis.html');
+    }
+  });
+}
+
 $(document).ready(function () {
   $('.button-main').click(function () {
-    $('#wrapper').fadeOut(500, function () {
-      let x = 0;
-      $('#wrapper').fadeIn(500, function () {
-        $('#carousel').fadeIn('slow');
-        //kode lanjutan
-        var page = ['dimsum', 'katsu_tempe', 'martabak_manis'];
-
-        var dimsum = [
-          'img/dimsum/IMG_20200409_094645_772.jpg',
-          'img/dimsum/IMG_20200409_094536_055.jpg',
-          'img/dimsum/IMG_20200409_094741_443.jpg',
-          'img/dimsum/IMG_20200409_094903_239.jpg',
-        ];
-        var katsu_tempe = [
-          'img/katsu-tempe/widyamarinil_20200605_1.png',
-          'img/katsu-tempe/widyamarinil_20200605_2.png',
-          'img/katsu-tempe/widyamarinil_20200605_3.png',
-          'img/katsu-tempe/widyamarinil_20200605_4.png',
-        ];
-        var martabak_manis = [
-          'img/martabak-manis/1.jpg',
-          'img/martabak-manis/2.jpg',
-          'img/martabak-manis/3.jpg',
-          'img/martabak-manis/4.jpg',
-          'img/martabak-manis/5.jpg',
-          'img/martabak-manis/6.jpg',
-          'img/martabak-manis/7.jpg',
-        ];
-        if (page[x] == 'dimsum') {
-          let this_page = dimsum;
-          images(this_page);
-        } else if (page[x] == 'katsu_tempe') {
-          let this_page = katsu_tempe;
-          images(this_page);
-        } else if (page[x] == 'martabak_manis') {
-          let this_page = martabak_manis;
-          images(this_page);
-        }
-      });
-
-      $('#wrapper').load('katsu-tempe.html');
-    });
+    change(hal);
   });
 });
-
 function images(page) {
   let value = 0;
   console.log(page[value]);
